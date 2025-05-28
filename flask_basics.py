4d3c26a0515afe9be30695cad924dbbf9d032043
Alvin <alvingeorge_@outlook.com>
2025-05-28 12:19:59 +0530

    added more html, css styles , bootstraps, etc , for efficent working

--- flask_basics.py
+++ flask_basics.py
@@ -1,11 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
+
+postdata = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return render_template('home.html', title='Its a home page')
+    return render_template('home.html', posts=postdata, title='Its a home page')
 
 
 @app.route('/about')
